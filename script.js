2fb1ec0eea1c59c3ff98fdf1026ab1ca515cabf5
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2026-02-08 17:18:10 -0300

    refactor: Add module DOM

--- script.js
+++ script.js
@@ -219,23 +219,32 @@ const modules = [
         theory: `
             <h2 class="text-3xl font-bold text-slate-800 mb-4">Funções</h2>
             <p class="text-slate-600 mb-4">Uma função é um código organizado e reaproveitado que é capaz de efetuar uma determinada ação.</p>
-            <img  src="./assets/arrays_js.svg" />
-            <p class="text-slate-600 mb-4">Métodos importantes:</p>
-            <ul class="list-disc ml-6 text-slate-600 mb-4">
-                <li><code>push()</code>: Adiciona ao final.</li>
-                <li><code>pop()</code>: Remove do final.</li>
-                <li><code>length</code>: Tamanho da lista.</li>
-            </ul>
+            <pre class="bg-slate-900 text-white p-4 rounded-lg text-sm mb-4">
+            function nomeDaFuncao(parametro1, parametro2){
+                // código
+            };
+            nomDaFuncao(argumento1, argumento2); // Chamada da função
+            </pre>
+            <p class="text-slate-600 mb-4">Outra maneira de declarar funções é usando uma expressão <strong>arrow funcions</strong> possui uma sintaxe mais curta. </p>
+            <pre class="bg-slate-900 text-white p-4 rounded-lg text-sm mb-4">
+                () => {
+                    // código
+                }
+            </pre>            
             <p class="text-slate-600 mb-4">Para conhecer mais sobre funções, acesse: <a href="https://javascript.info/function-basics" class="text-blue-500 hover:underline" target="_blank">Funções em JavaScript</a></p>
         `,
-        initialCode: `let frutas = ["Maçã", "Banana", "Laranja"];
-
-        console.log("Lista completa:", frutas);
-        console.log("Primeira fruta:", frutas[0]);
-
-        frutas.push("Uva");
-        console.log("Após o push:", frutas);
-        console.log("Total de itens:", frutas.length);`
+        initialCode: `function soma_nota(nota1, nota2) {
+        return nota1 + nota2;
+        }
+        const nota_final = soma_nota(8, 7);
+        console.log("Nota final:", nota_final);
+        
+        /* Exemplo com arrow functions */
+        
+        let elementos_quimicos = ["Lítio","Sódio"];
+        elementos_quimicos.map(
+            (elemento) => console.log(elemento)
+        );`
     },
     {
         id: 'dom',
@@ -244,6 +253,9 @@ const modules = [
         theory: `
             <h2 class="text-3xl font-bold text-slate-800 mb-4">Manipulação da DOM</h2>
             <p class="text-slate-600 mb-4">A DOM (Document Object Model) é a representação do HTML que o JavaScript pode acessar.</p>
+            <p class="text-slate-600 mb-4">Vamos começar com um exemplo bem simples:</p>
+            <img  src="./assets/dom.svg" class="mb-2" />
+            
             <div class="bg-amber-50 p-4 border-l-4 border-amber-500 mb-6">
                 <p class="text-amber-800 font-medium">Clique no botão abaixo para abrir o Laboratório Visual!</p>
                 <button onclick="document.getElementById('dom-sandbox-container').classList.remove('hidden')" class="mt-3 bg-amber-600 text-white px-4 py-2 rounded font-bold hover:bg-amber-700">Abrir Lab de DOM</button>
@@ -255,6 +267,7 @@ const modules = [
                 <li><code>element.style.color</code></li>
                 <li><code>element.addEventListener()</code></li>
             </ul>
+            <p class="text-slate-600 mb-4">Para conhecer mais sobre manipulação da DOM, acesse: <a href="https://developer.mozilla.org/pt-BR/docs/Web/API/Document_Object_Model/" class="text-blue-500 hover:underline" target="_blank">DOM em JavaScript</a></p>
         `,
         initialCode: `// No console abaixo não mudamos o HTML visível,
             // apenas simulamos a lógica de seleção.
